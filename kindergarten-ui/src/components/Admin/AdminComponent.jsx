019af8234db968ba2b1a7f3635ad7ea2b7152869
Paulius Savickis <paulsavickis@gmail.com>
2021-01-22 18:49:19 +0200

    fixed React's For and Manifest warnings

--- kindergarten-ui/src/components/Admin/AdminComponent.jsx
+++ kindergarten-ui/src/components/Admin/AdminComponent.jsx
@@ -10,11 +10,11 @@ let AdminComponent = (props) => {
             <h1>Prisijungimas</h1>
             <form className="form">
                 <div className="form-group">
-                    <label for="username">Prisijungimo vardas: </label>
+                    <label htmlFor="username">Prisijungimo vardas: </label>
                     <input className="form-control " id="username" value={props.username} onChange={props.onUsernameChange} placeholder="Prisijungimo vardas"></input>
                 </div>
                 <div className="form-group">
-                    <label for="password" className="control-label" >Slaptažodis: </label>
+                    <label htmlFor="password" className="control-label" >Slaptažodis: </label>
                     <input className="form-control" type="password" id="password" value={props.password} onChange={props.onPasswordChange} placeholder="Slaptažodis"></input>
                 </div>  
             <button className="btn btn-primary" onClick={props.onSubmit}>Prisijungti</button>
