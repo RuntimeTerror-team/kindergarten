47926299918d5e3aae2a8c066c6eb13f7c66af8f
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-01-22 22:24:26 +0200

    removed bootstrap from index.html, temporarily added form to App.js

--- kindergarten-ui/src/App.js
+++ kindergarten-ui/src/App.js
@@ -1,12 +1,11 @@
+import AdminLoginContainer from "./components/AdminLogin/AdminLoginContainer";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-lg-4 offset-lg-4">
-              <h1>React aplikacija veikia, jei matote šitą antraštę</h1>
-          </div>
+          <AdminLoginContainer />
         </div>
       </div>
     </div>
