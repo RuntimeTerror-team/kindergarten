83a12dce3455698d38b8a90fe5bddc657f8a41ea
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-01-23 23:07:46 +0200

    Fixed issue in frontend

--- kindergarten-ui/src/components/Pages/AdminPage.jsx
+++ kindergarten-ui/src/components/Pages/AdminPage.jsx
@@ -1,21 +1,20 @@
-import React, { useContext, useState, useEffect } from 'react';
+// import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import ServicesContext from '../../context/ServicesContext';
 
 const AdminPage = () => {
     const { userService } = useContext(ServicesContext);
-    const [userRole, setUserRole] = useState(userService.getUserRole());
+    // const [userRole, setUserRole] = useState(userService.getUserRole());
 
-    userService.updateUserRole = () => setUserRole(userService.getUserRole());
+    // userService.updateUserRole = () => setUserRole(userService.getUserRole());
 
-    useEffect(() => {
-        if (userRole.toLowerCase() === "ADMIN") {
-            setUserRole("ADMIN")
-        }
-    }, [userRole])
+    // useEffect(() => {
+    //     setUserRole(userService.getUserRole())
+    // }, [userRole])
 
     return (
     <div>
-        {userRole === "ADMIN" ? <h1>Admin page</h1> : <h1>Access denied</h1>}
+        {userService.getUserRole() === "ADMIN" ? <h1>Admin page</h1> : <h1>Access denied</h1>}
     </div>
     )
 }
