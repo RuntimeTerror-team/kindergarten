28938ee5b646d7e7c7ebf61513035ac9b279fe3c
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-01-25 15:40:45 +0200

    Removed second user which was created for testing

--- kindergarten/src/main/java/lt/vtmc/kindergarten/CommandLineAppStartupRunner.java
+++ kindergarten/src/main/java/lt/vtmc/kindergarten/CommandLineAppStartupRunner.java
@@ -1,8 +1,5 @@
 package lt.vtmc.kindergarten;
 
-import lt.vtmc.kindergarten.dao.UserDao;
-import lt.vtmc.kindergarten.domain.Role;
-import lt.vtmc.kindergarten.domain.RoleType;
 import lt.vtmc.kindergarten.dto.UserFromService;
 import lt.vtmc.kindergarten.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +11,8 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     @Autowired
     UserService userService;
 
-    @Autowired
-    UserDao userDao;
-
     @Override
     public void run(String...args) throws Exception {
         userService.createUser(new UserFromService("administratorius", "firstname", "lastname", 123456789L, "Administratorius1", "ADMIN"));
-        userService.createUser(new UserFromService("asas", "firstname", "lastname", 123456789L, "asas", "ADMIN"));
     }
 }
