1d85e07df86ec121a75e47bfece86bf4a1b6d3fa
gnar-404 <75095108+gnar-404@users.noreply.github.com>
2021-01-27 22:03:20 +0200

    uncomment application

--- kindergarten/src/main/java/lt/vtmc/kindergarten/dao/Application.java
+++ kindergarten/src/main/java/lt/vtmc/kindergarten/dao/Application.java
@@ -3,6 +3,6 @@ package lt.vtmc.kindergarten.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface Application extends JpaRepository<Application, Long> {
-//}
+@Repository
+public interface Application extends JpaRepository<Application, Long> {
+}
