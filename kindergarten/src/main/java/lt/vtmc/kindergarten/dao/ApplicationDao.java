e44d57c535c95580772f79f09638401b9fa9f11a
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-01-27 22:17:47 +0200

    fix dao class type and import

---
+++ kindergarten/src/main/java/lt/vtmc/kindergarten/dao/ApplicationDao.java
@@ -0,0 +1,10 @@
+package lt.vtmc.kindergarten.dao;
+
+import lt.vtmc.kindergarten.domain.Application;
+import lt.vtmc.kindergarten.domain.Child;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationDao extends JpaRepository<Application, Long> {
+}
