0a4846651ea390c34c1150f17f30791cd7229f8e
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-02-01 20:57:09 +0200

    Added district administration files and font-awesome dependencies

--- kindergarten-ui/src/components/AdminPage/AdminPageComponent.jsx
+++ kindergarten-ui/src/components/AdminPage/AdminPageComponent.jsx
@@ -10,13 +10,13 @@ const AdminPageComponent = ({handleLogout, handleUserChoice, choice}) => {
                 <div className="row">
                     <div className="admin-actions col-4">
                         <button className="btn btn-info mb-2 w-100" onClick={handleUserChoice} name="adminUsers">Paskyrų administravimas</button>
-                        <button className="btn btn-info mb-2 w-100" onClick={handleUserChoice} name="2">Kažkas dar</button>
+                        <button className="btn btn-info mb-2 w-100" onClick={handleUserChoice} name="adminDistricts">Rajonų administravimas</button>
                         <button className="btn btn-info mb-2 w-100" onClick={handleUserChoice} name="3">Ir dar kažkas</button>
                     </div>
                     <div className="admin-action-placeholder col-8">
                         {choice === "greeting" && <h1>Sveiki prisijungę į darželių informacinę sistemą</h1>}
                         {choice === "adminUsers" && <AdminUsersFormContainer />}
-                        {choice === "2" && <h1>Kažkas dar</h1>}
+                        {choice === "adminDistricts" && <h1>Rajonų administravimas</h1>}
                         {choice === "3" && <h1>Ir dar kažkas</h1>}
                     </div>
                 </div>
