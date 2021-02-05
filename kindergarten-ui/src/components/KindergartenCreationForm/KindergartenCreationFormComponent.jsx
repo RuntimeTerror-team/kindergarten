a9581175ba77d7a04d2401e7f7a2662fc8cd66c5
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-02-05 14:13:47 +0200

    Added full kindergarten creation form validation

--- kindergarten-ui/src/components/KindergartenCreationForm/KindergartenCreationFormComponent.jsx
+++ kindergarten-ui/src/components/KindergartenCreationForm/KindergartenCreationFormComponent.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 
-const KindergartenCreationFormComponent = ({ street, buildingNo, email, phoneNo, postalCode, title, website, districts, handleChange, companyCode, handleSubmit, stopCreatingKindergarten, titleValidation }) => {
+const KindergartenCreationFormComponent = ({ address, email, phoneNo, postalCode, title, website, districts, handleChange, companyCode, handleSubmit, stopCreatingKindergarten, failMessage, failMessageStyle, titleValidation, companyCodeValidation, addressValidation, districtValidation, postalCodeValidation, phoneNoValidation, emailValidation,websiteValidation }) => {
     return (
         <div className="col-12">
             <form onSubmit={handleSubmit}>
                 <div className="form-group row">
                     <label htmlFor="title" className="col-3 pt-2 text-right">Pavadinimas</label>
                     <input type="text" className={`form-control col-9 ${titleValidation}`} id="title" placeholder="Pvz.: Lopšelis-darželis Voverytė" value={title} name="title" onChange={handleChange} />
-                    <div class="invalid-feedback">
-                       Šis laukas privalomas. Pavadinimo ilgis turi būti 8-35 ženklų.
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Šis laukas privalomas. Pavadinimo ilgis turi būti 8-35 ženklų.
                     </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="companyCode" className="col-3 pt-2 text-right">Įmonės kodas</label>
-                    <input type="number" className="form-control col-9" id="companyCode" placeholder="Pvz.: 123456789" value={companyCode} name="companyCode" onChange={handleChange} />
+                    <input type="number" className={`form-control col-9 ${companyCodeValidation}`} id="companyCode" placeholder="Pvz.: 123456789" value={companyCode} name="companyCode" onChange={handleChange} />
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Šis laukas privalomas. Įmonės kodo ilgis turi būti 7 arba 9 ženklų.
+                    </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="address" className="col-3 pt-2 text-right">Adresas</label>
-                    <input type="text" className="form-control col-7" id="address" placeholder="Pvz.: Kalvarijų g." value={street} name="street" onChange={handleChange} />
-                    <input type="number" min="1" className="form-control col-2" placeholder="Namo nr." value={buildingNo} name="buildingNo" onChange={handleChange} />
+                    <input type="text" className={`form-control col-9 ${addressValidation}`} id="address" placeholder="Pvz.: Kalvarijų g. 128" value={address} name="address" onChange={handleChange} />
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Šis laukas privalomas. Adreso ilgis turi būti 8-50 ženklų.
+                    </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="district" className="col-3 pt-2 text-right">Rajonas</label>
-                    <select id="district" className="form-control col-9" name="district" onChange={handleChange} >
+                    <select id="district" className={`form-control col-9 ${districtValidation}`} name="district" onChange={handleChange} >
                         <option defaultValue>Pasirinkti...</option>
                         {districts.map(({ title, id }) => {
                             return (
@@ -30,6 +38,10 @@ const KindergartenCreationFormComponent = ({ street, buildingNo, email, phoneNo,
                             )
                         })}
                     </select>
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Šis laukas privalomas. Pasirinkite rajoną.
+                    </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="city" className="col-3 pt-2 text-right">Miestas</label>
@@ -38,24 +50,41 @@ const KindergartenCreationFormComponent = ({ street, buildingNo, email, phoneNo,
                 <div className="form-group row">
                     <label htmlFor="postalCode" className="col-3 pt-2 text-right">Pašto kodas</label>
                     <input type="text" className="form-control col-1" placeholder="LT-" readOnly />
-                    <input type="number" className="form-control col-8" id="postalCode" placeholder="00000" value={postalCode} name="postalCode" onChange={handleChange} />
+                    <input type="number" className={`form-control col-8 ${postalCodeValidation}`} id="postalCode" placeholder="00000" value={postalCode} name="postalCode" onChange={handleChange} />
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Šis laukas privalomas. Pašto kodo ilgis turi būti 5 ženklų.
+                    </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="phoneNo" className="col-3 pt-3 text-right">Tel. numeris</label>
                     <input type="text" className="form-control col-1 p-1" placeholder="+370" readOnly />
-                    <input type="text" className="form-control col-8" id="phoneNo" placeholder="60000000" value={phoneNo} name="phoneNo" onChange={handleChange} />
+                    <input type="text" className={`form-control col-8 ${phoneNoValidation}`} id="phoneNo" placeholder="60000000" value={phoneNo} name="phoneNo" onChange={handleChange} />
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Šis laukas privalomas. Įveskite 8 skaitmenis.
+                    </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="email" className="col-3 pt-2 text-right">El. paštas</label>
-                    <input type="email" className="form-control col-9" id="email" placeholder="pavyzdys@pvz.lt" value={email} name="email" onChange={handleChange} />
+                    <input type="email" className={`form-control col-9 ${emailValidation}`} id="email" placeholder="pavyzdys@pvz.lt" value={email} name="email" onChange={handleChange} />
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Įveskite elektroninį paštą arba palikite tuščią.
+                    </div>
                 </div>
                 <div className="form-group row">
                     <label htmlFor="website" className="col-3 pt-2 text-right">Interneto svetainė</label>
-                    <input type="url" className="form-control col-9" id="website" placeholder="www.pavyzdys.lt" value={website} name="website" onChange={handleChange} />
+                    <input type="url" className={`form-control col-9 ${websiteValidation}`} id="website" placeholder="www.pavyzdys.lt" value={website} name="website" onChange={handleChange} />
+                    <div className="col-3"></div>
+                    <div className="invalid-feedback col-9">
+                        Įveskite svetainės adresą arba palikite tuščią.
+                    </div>
                 </div>
                 <button type="submit" className="btn btn-info float-right">Išsaugoti</button>
             </form>
             <button className="btn btn-warning float-right mx-2" onClick={stopCreatingKindergarten}>Atšaukti</button>
+            {<span className={`${failMessageStyle} alertTop`} style={{ width: "23em" }}>{failMessage}</span>}
         </div>
     )
 }
