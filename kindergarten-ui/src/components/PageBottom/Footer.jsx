9afd7eddc22edee1481d0724801f4e06ef404c55
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-02-08 14:54:12 +0200

    change UI desighn

---
+++ kindergarten-ui/src/components/PageBottom/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer py-4 text-white-50">
+      <div className="container text-center">
+        <small>Copyright &copy; RuntimeTerror</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
