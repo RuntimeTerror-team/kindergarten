3ffe8915b3f64838660203a56090893ee12aa36c
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-02-13 20:21:03 +0200

    Updated error pages

--- kindergarten-ui/src/components/ErrorHandling/Error.jsx
+++ kindergarten-ui/src/components/ErrorHandling/Error.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Error = (props) => {
-    const goApp = () => props.history.goBack();
+    const goApp = () => props.history.push("/");
     return (
         <div className="container">
             <div className='m-5'>
                 <h1>Klaida</h1>
-                <button className='btn btn-primary mt-3' onClick={goApp}>Grįžti</button>
+                <button className='btn btn-primary mt-3' onClick={goApp}>Eiti į pagrindinį puslapį</button>
             </div>
         </div>
     )
--- kindergarten-ui/src/components/ErrorHandling/Forbidden.jsx
+++ kindergarten-ui/src/components/ErrorHandling/Forbidden.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Forbidden = (props) => {
     //403
-    const goBack = () => props.history.goBack();
+    const goApp = () => props.history.push("/");
     return (
         <div className="container">
             <div className='m-5'>
                 <h1>Prieiga uždrausta</h1>
                 <p> Neturite leidimo šiai užklausai.</p>
-                <button className='btn btn-primary mt-3' onClick={goBack}>Grįžti</button>
+                <button className='btn btn-primary mt-3' onClick={goApp}>Eiti į pagrindinį puslapį</button>
             </div>
         </div>
     )
