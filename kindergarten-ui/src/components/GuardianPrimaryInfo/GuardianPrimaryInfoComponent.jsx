3a3d254a950950c4cf8e9a86acddd8771b758ad4
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-02-16 15:04:59 +0200

    Updated info message

--- kindergarten-ui/src/components/GuardianPrimaryInfo/GuardianPrimaryInfoComponent.jsx
+++ kindergarten-ui/src/components/GuardianPrimaryInfo/GuardianPrimaryInfoComponent.jsx
@@ -7,7 +7,7 @@ const GuardianPrimaryInfoComponent = () => {
                 <h4 className="alert-heading">Sveiki prisijungę!</h4>
                 <p> Prašome suvesti registracijos duomenis, kad galėtumėte pildyti prašymus ir naudotis kitomis svetainės funkcijomis. Jūsų duomenis tvarkysime tik prašymų į darželius ir susijusiais klausimais.</p>
                 <hr />
-                <p className="mb-0">Jei vėliau pageidausite nebepildyti prašymų ir/ar nebesinaudoti svetainės funkcijomis, jūsų duomenys galės būti ištrinti.</p>
+                <p className="mb-0">Jei vėliau pageidausite nepildyti prašymų ir/ar nesinaudoti svetainės funkcijomis, jūsų duomenys galės būti ištrinti.</p>
             </div>
         </div>
     )
