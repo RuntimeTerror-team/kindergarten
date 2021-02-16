5d1151c66df48bf7857fdb3f21909c639ae6c080
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-02-16 20:29:33 +0200

    Added bcrypt password encoder

--- kindergarten/src/main/java/lt/vtmc/kindergarten/security/PasswordConfig.java
+++ kindergarten/src/main/java/lt/vtmc/kindergarten/security/PasswordConfig.java
@@ -2,15 +2,16 @@ package lt.vtmc.kindergarten.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
-    @SuppressWarnings("deprecation")
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new
-                MessageDigestPasswordEncoder("SHA-256");
+//        return new
+//                MessageDigestPasswordEncoder("SHA-256");
+        return new BCryptPasswordEncoder();
     }
 }
