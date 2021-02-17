3c817a2c7c3faebbe89d4682e585964e63381702
Elbromistafalso <61710401+Elbromistafalso@users.noreply.github.com>
2021-02-17 12:46:51 +0200

    Update ApplicationController.java
    removed ambiguity of GET requests

--- kindergarten/src/main/java/lt/vtmc/kindergarten/controller/ApplicationController.java
+++ kindergarten/src/main/java/lt/vtmc/kindergarten/controller/ApplicationController.java
@@ -29,7 +29,7 @@ public class ApplicationController {
         applicationService.addApplication(applicationCreationDto);
     }
     
-    @RequestMapping(value="/api/applications", method = RequestMethod.GET)
+    @RequestMapping(value="/api/applicationsDtos", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Get applications", notes = "Returns a list of applications")
     public List<ApplicationDto> getApplicationsList(){
