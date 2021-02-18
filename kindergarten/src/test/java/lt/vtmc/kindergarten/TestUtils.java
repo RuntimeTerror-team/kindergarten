269729c2ed51a4205ed94c9d04a36769d5e75f4a
gnar-404 <75095108+gnar-404@users.noreply.github.com>
2021-02-18 10:12:19 +0200

    add queue in db (#80)

--- kindergarten/src/test/java/lt/vtmc/kindergarten/TestUtils.java
+++ kindergarten/src/test/java/lt/vtmc/kindergarten/TestUtils.java
@@ -6,69 +6,71 @@ import lt.vtmc.kindergarten.dto.ApplicationCreationDto;
 import lt.vtmc.kindergarten.dto.QueueDtoFromAdmin;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public final class TestUtils {
 
-        public static District createDefaultDistrict(String title){
-            District district = new District();
-            district.setTitle(title);
-           return  district;
-        }
+    public static District createDefaultDistrict(String title) {
+        District district = new District();
+        district.setTitle(title);
+        return district;
+    }
 
-        public static Kindergarten createDefaultKindergarten(String companyCode){
-            Kindergarten kindergarten = new Kindergarten();
-            kindergarten.setTitle("Pušaitė");
-            kindergarten.setAddress("Gatves g. 56");
-            kindergarten.setCity(CityEnum.VILNIUS);
-            kindergarten.setPostalCode("12546");
-            kindergarten.setPhoneNumber("862403523");
-            kindergarten.setEmail("darzelispusaite@gmail.com");
-            kindergarten.setWebsite("www.darzelis.lt");
-            kindergarten.setCompanyCode(companyCode);
+    public static Kindergarten createDefaultKindergarten(String companyCode) {
+        Kindergarten kindergarten = new Kindergarten();
+        kindergarten.setTitle("Pušaitė");
+        kindergarten.setAddress("Gatves g. 56");
+        kindergarten.setCity(CityEnum.VILNIUS);
+        kindergarten.setPostalCode("12546");
+        kindergarten.setPhoneNumber("862403523");
+        kindergarten.setEmail("darzelispusaite@gmail.com");
+        kindergarten.setWebsite("www.darzelis.lt");
+        kindergarten.setCompanyCode(companyCode);
 
-            return kindergarten;
-        }
+        return kindergarten;
+    }
 
-        public static Person createDefaultPerson(String personalCode){
-            Person person = new Person();
-            person.setFirstName("Antanas");
-            person.setLastName("Antanaitis");
-            person.setPersonalCode(personalCode);
-            person.setAddress("Kanklių g.4");
-            person.setCity(CityEnum.VILNIUS);
-            person.setPostalCode("12355");
-            return person;
-        }
+    public static Person createDefaultPerson(String personalCode) {
+        Person person = new Person();
+        person.setFirstName("Antanas");
+        person.setLastName("Antanaitis");
+        person.setPersonalCode(personalCode);
+        person.setAddress("Kanklių g.4");
+        person.setCity(CityEnum.VILNIUS);
+        person.setPostalCode("12355");
+        return person;
+    }
 
-        public static ApplicationCreationDto createDefaultApplicationDto(){
-            ApplicationCreationDto applicationDto = new ApplicationCreationDto();
-            applicationDto.setDate(new Date());
-            applicationDto.setIsAdopted(true);
-            applicationDto.setIsGuardianDisabled(false);
-            applicationDto.setIsMultiChild(false);
-            applicationDto.setIsGuardianStudent(false);
+    public static ApplicationCreationDto createDefaultApplicationDto() {
+        ApplicationCreationDto applicationDto = new ApplicationCreationDto();
+        applicationDto.setDate(new Date());
+        applicationDto.setIsAdopted(true);
+        applicationDto.setIsGuardianDisabled(false);
+        applicationDto.setIsMultiChild(false);
+        applicationDto.setIsGuardianStudent(false);
 
-            return applicationDto;
-        }
+        return applicationDto;
+    }
 
-        public static Group createDefaultGroup(Kindergarten kindergarten){
-            Group group = new Group();
-            AgeRange ageRange = new AgeRange();
-            ageRange.setAgeMin(1);
-            ageRange.setAgeMax(2);
-            group.setAgeRange(ageRange);
-            group.setKindergartenId(kindergarten);
-            group.setChildrenCount(10);
-            group.setTitle("Pukelis");
+    public static Group createDefaultGroup(Kindergarten kindergarten) {
+        Group group = new Group();
+        AgeRange ageRange = new AgeRange();
+        ageRange.setAgeMin(1);
+        ageRange.setAgeMax(2);
+        group.setAgeRange(ageRange);
+        group.setKindergartenId(kindergarten);
+        group.setChildrenCount(10);
+        group.setTitle("Pukelis");
 
-            return group;
-        }
+        return group;
+    }
 
-        public static QueueDtoFromAdmin createDefaultQueue(){
-            QueueDtoFromAdmin queue = new QueueDtoFromAdmin();
-            queue.setOpeningDate(new Date());
-            return queue;
-        }
+    public static QueueDtoFromAdmin createDefaultQueue() {
+        QueueDtoFromAdmin queue = new QueueDtoFromAdmin();
+        queue.setOpeningDate(new Date());
+        return queue;
+    }
 
 }
