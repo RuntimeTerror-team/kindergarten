49ab1e527897205b8fa51cdb05824a7a9c3d8a64
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-02-23 22:45:09 +0200

    partial

--- kindergarten/src/test/java/lt/vtmc/kindergarten/AlgorithmTest.java
+++ kindergarten/src/test/java/lt/vtmc/kindergarten/AlgorithmTest.java
@@ -21,7 +21,7 @@ public class AlgorithmTest {
     @DisplayName("counting Child Age")
     public void countChildAge() {
 
-        String personalCode = "61601182111";
+        String personalCode = "61501223111";
 
         String birthdayYear = personalCode.substring(1, 3);
         String birthdayMonth = personalCode.substring(3, 5);
@@ -37,58 +37,56 @@ public class AlgorithmTest {
         String currentDay = formattedString.substring(8, 10);
 
 
+        //PASKAICIUOTAS VAIKO AMZIUS DABAR
+        String childAgeInYearsNow = "";
+        String childAgeInMonthNow = "";
+        String childAgeInDaysNow = "";
 
 
-//        childAgeYear = Integer.toString((Integer.parseInt(currentYear) - Integer.parseInt(birthdayYear)));
-//        childAgeMonth = Integer.toString((1 - Integer.parseInt(birthdayMonth)));
+        if (Integer.parseInt(currentMonth) == Integer.parseInt(birthdayMonth)) {
+            if (Integer.parseInt(currentDay) >= Integer.parseInt(birthdayDay)) {
+                childAgeInYearsNow = Integer.toString(Integer.parseInt(currentYear) - Integer.parseInt(birthdayYear));
+            } else {
+                childAgeInYearsNow = Integer.toString(Integer.parseInt(currentYear) - Integer.parseInt(birthdayYear) - 1);
+            }
+            childAgeInMonthNow = Integer.toString(Integer.parseInt(currentMonth) - Integer.parseInt(birthdayMonth));
+        } else if ((Integer.parseInt(currentMonth) > Integer.parseInt(birthdayMonth))) {
+            childAgeInYearsNow = Integer.toString((Integer.parseInt(currentYear) - Integer.parseInt(birthdayYear)));
+            childAgeInMonthNow = Integer.toString(Integer.parseInt(currentMonth) - Integer.parseInt(birthdayMonth));
+        } else if ((Integer.parseInt(currentMonth) < Integer.parseInt(birthdayMonth))) {
+            childAgeInYearsNow = Integer.toString((Integer.parseInt(currentYear) - Integer.parseInt(birthdayYear))-1);
+            childAgeInMonthNow = Integer.toString(Integer.parseInt(birthdayMonth) - Integer.parseInt(currentMonth));
+        }
 
 
-        String childAgeYear = "";
-        String childAgeMonth = "";
-
-        int childAgeYearInteger = (Integer.parseInt(currentYear) - Integer.parseInt(birthdayYear));
-        int childAgeMonthInteger = (12 - Integer.parseInt(birthdayMonth) + 9);
+        assertEquals("6", childAgeInYearsNow, "age");
+        assertEquals("1", childAgeInMonthNow, "month");
 
 
         //PASKAICIUOTAS VAIKO AMZIUS ARTIMIAUSIA RUGSEJI
-        if(childAgeMonthInteger > 12){
-            childAgeYearInteger += 1;
-            childAgeYear = Integer.toString(childAgeYearInteger);
-            childAgeMonthInteger = 0;
-            childAgeMonth = Integer.toString(childAgeMonthInteger);
-        } else {
-            childAgeYear = Integer.toString(childAgeYearInteger);
-            childAgeMonth = Integer.toString(childAgeMonthInteger);
-
-        }
-
-//        if (childAgeMonthInteger < 0) {
-//            childAgeMonthInteger = ((childAgeMonthInteger * (-1)) + 9);
-//            if (childAgeMonthInteger > 12) {
-//                childAgeYearInteger = childAgeYearInteger +1;
-//                childAgeYear = Integer.toString(childAgeYearInteger);
-//                childAgeMonthInteger = childAgeMonthInteger - 9;
-//                childAgeMonth = Integer.toString(childAgeMonthInteger);
-//            }
-//
+//        if(childAgeMonthInteger > 12){
+//            childAgeYearInteger += 1;
+//            childAgeYear = Integer.toString(childAgeYearInteger);
+//            childAgeMonthInteger = 0;
+//            childAgeMonth = Integer.toString(childAgeMonthInteger);
 //        } else {
 //            childAgeYear = Integer.toString(childAgeYearInteger);
-//            childAgeMonthInteger = childAgeMonthInteger + 9;
 //            childAgeMonth = Integer.toString(childAgeMonthInteger);
+//
 //        }
 
 
 //        assertEquals("16", birthdayYear, "Should return the right year");
-//        assertEquals("03", birthdayMonth, "Should return the right month");
+//        assertEquals("02", birthdayMonth, "Should return the right month");
 //        assertEquals("18", birthdayDay, "Should return the right day");
-
-        assertEquals("5", childAgeYear, "Should return the correct child age");
-        assertEquals("0", childAgeMonth, "Should return the correct child age");
-
-        assertEquals("2021/02/23", formattedString, "Should return whole current year");
-        assertEquals("21", currentYear, "Should return the current year");
-        assertEquals("02", currentMonth, "Should return the current month");
-        assertEquals("23", currentDay, "Should return the current day");
+//
+////        assertEquals("5", childAgeYear, "Should return the correct child age");
+////        assertEquals("0", childAgeMonth, "Should return the correct child age");
+//
+//        assertEquals("2021/02/23", formattedString, "Should return whole current year");
+//        assertEquals("21", currentYear, "Should return the current year");
+//        assertEquals("02", currentMonth, "Should return the current month");
+//        assertEquals("23", currentDay, "Should return the current day");
 
     }
 
