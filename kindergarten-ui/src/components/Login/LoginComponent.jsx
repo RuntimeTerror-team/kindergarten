5bfacd06f49e7801db2da554fc9a78e2444b8c27
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-02-28 03:38:13 +0200

    partial commit

--- kindergarten-ui/src/components/Login/LoginComponent.jsx
+++ kindergarten-ui/src/components/Login/LoginComponent.jsx
@@ -1,44 +1,64 @@
 import React from "react";
 import Proptypes from "prop-types";
+import xxx from "../../image/xxx.png";
+import "../../styles/xxx.css";
 
-let LoginComponent = ({ username, password, usernameValidation, passwordValidation, areCredentialsIncorrect,
-  onSubmit, onUsernameChange, onPasswordChange }) => {
-
+let LoginComponent = ({
+  username,
+  password,
+  usernameValidation,
+  passwordValidation,
+  areCredentialsIncorrect,
+  onSubmit,
+  onUsernameChange,
+  onPasswordChange,
+}) => {
   return (
-    <div className="row" id="loginForm">
-      <div>
-        <h1 id="loginh1">Prisijungimas</h1>
-        <form className="form" onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Prisijungimo vardas: </label>
-            <input className={`largeInput form-control ${usernameValidation}`} id="username" value={username} onChange={onUsernameChange} name="username"></input>
-            <div className="invalid-feedback">
-              Šis laukas privalomas.
-                        </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password" className="control-label" >Slaptažodis: </label>
-            <input className={`largeInput form-control ${passwordValidation}`} type="password" id="password" value={password} onChange={onPasswordChange} name="password"></input>
-            <div className="invalid-feedback">
-              Šis laukas privalomas.
-                        </div>
-          </div>
-          <button className="btn btn-primary mb-4" id="loginButton">Prisijungti</button>
-        </form>
-        {areCredentialsIncorrect && (
-          <div className="alert alert-danger col-12" role="alert">
-            Prisijungimo duomenys neteisingi
-          </div>
-        )}
+    <div className="pt-4">
+      <div className="row" id="loginForm">
+        <div>
+          <h3 className="text-center text-info pt"> Vaikų darželių informacinė sistema</h3>
+          <h3 id="loginh1">Prisijungimas</h3>
+          <form className="form" onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Prisijungimo vardas: </label>
+              <input
+                className={`largeInput form-control ${usernameValidation}`}
+                id="username"
+                value={username}
+                onChange={onUsernameChange}
+                name="username"></input>
+              <div className="invalid-feedback">Šis laukas privalomas.</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="control-label">
+                Slaptažodis:{" "}
+              </label>
+              <input
+                className={`largeInput form-control ${passwordValidation}`}
+                type="password"
+                id="password"
+                value={password}
+                onChange={onPasswordChange}
+                name="password"></input>
+              <div className="invalid-feedback">Šis laukas privalomas.</div>
+            </div>
+            <button className="btn btn-primary mb-4" id="loginButton">
+              Prisijungti
+            </button>
+          </form>
+          {areCredentialsIncorrect && (
+            <div className="alert alert-danger col-12" role="alert">
+              Prisijungimo duomenys neteisingi
+            </div>
+          )}
+        </div>
       </div>
-
     </div>
-  )
-
-}
+  );
+};
 
 LoginComponent.propTypes = {
-
   username: Proptypes.string.isRequired,
   password: Proptypes.string.isRequired,
   usernameValidation: Proptypes.string.isRequired,
@@ -46,8 +66,7 @@ LoginComponent.propTypes = {
   areCredentialsIncorrect: Proptypes.bool.isRequired,
   onUsernameChange: Proptypes.func.isRequired,
   onPasswordChange: Proptypes.func.isRequired,
-  onSubmit: Proptypes.func.isRequired
-
-}
+  onSubmit: Proptypes.func.isRequired,
+};
 
 export default LoginComponent;
--- kindergarten-ui/src/components/Login/LoginContainer.jsx
+++ kindergarten-ui/src/components/Login/LoginContainer.jsx
@@ -1,150 +1,175 @@
-import axios from 'axios';
-import React, { Component } from 'react';
+import axios from "axios";
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import LoginComponent from './LoginComponent';
+import LoginComponent from "./LoginComponent";
 import baseUrl from "../../AppConfig";
-import urls from '../../constants/urls';
+import urls from "../../constants/urls";
+import xxx from "../../image/xxx.png";
+import "../../styles/xxx.css";
 
 axios.defaults.withCredentials = true;
 
 class LoginContainer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "",
-            password: "",
-            usernameValidation: "",
-            passwordValidation: "",
-            areCredentialsIncorrect: false,
-            userRole: ""
-        }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      usernameValidation: "",
+      passwordValidation: "",
+      areCredentialsIncorrect: false,
+      userRole: "",
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(`${baseUrl}/loggedRole`)
+      .then((res) => {
+        this.setState({ userRole: res.data });
+      })
+      .then(() => {
+        this.checkLoggedIn();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  checkLoggedIn = () => {
+    if (this.state.userRole === "ROLE_ADMIN") {
+      this.props.history.push(urls.admin.userBase);
+    } else if (this.state.userRole === "ROLE_EDUCATION_SPECIALIST") {
+      this.props.history.push(urls.educationSpecialist.kindergartenBase);
+    } else if (this.state.hasDetails && this.state.userRole === "ROLE_GUARDIAN") {
+      this.props.history.push(urls.guardian.applicationBase);
+    } else if (!this.state.hasDetails && this.state.userRole === "ROLE_GUARDIAN") {
+      this.props.history.push(urls.guardian.primaryDataBase);
+    }
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+
+    if (this.state.usernameValidation !== "" && name === "username") {
+      this.setState({ usernameValidation: "" });
     }
 
-    componentDidMount = () => {
-        axios
+    if (this.state.passwordValidation !== "" && name === "password") {
+      this.setState({ passwordValidation: "" });
+    }
+
+    if (this.state.areCredentialsIncorrect) {
+      this.setState({ areCredentialsIncorrect: false });
+    }
+  };
+
+  resetState = () => {
+    this.setState({ username: "" });
+    this.setState({ password: "" });
+    this.setState({ usernameValidation: "" });
+    this.setState({ passwordValidation: "" });
+    this.setState({ incorrectCredentials: false });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let usernameFromUser = e.target.username.value;
+    let passwordFromUser = e.target.password.value;
+
+    this.doValidation(usernameFromUser, passwordFromUser);
+
+    if (usernameFromUser.trim().length !== 0 && passwordFromUser.trim().length !== 0) {
+      let userData = new URLSearchParams();
+      userData.append("username", this.state.username);
+      userData.append("password", this.state.password);
+
+      axios
+        .post(`${baseUrl}/login`, userData, { headers: { "Content-type": "application/x-www-form-urlencoded" } })
+        .then(() => {
+          axios
             .get(`${baseUrl}/loggedRole`)
             .then((res) => {
-                this.setState({ userRole: res.data })
-            })
-            .then(() => {
+              this.setState({ userRole: res.data });
+              if (res.data === "ROLE_GUARDIAN") {
+                axios
+                  .get(`${baseUrl}/loggedWithDetails`)
+                  .then((res) => {
+                    console.log(res.data);
+                    this.setState({ hasDetails: res.data });
+                  })
+                  .then(() => this.checkLoggedIn())
+                  .catch((err) => console.log(err));
+              } else {
                 this.checkLoggedIn();
+              }
             })
-            .catch(err => console.log(err))
+            .catch((err) => console.log(err));
+        })
+        .catch((e) => {
+          if (e.response.status && e.response.status === 401) {
+            this.setState({ areCredentialsIncorrect: true });
+          } else {
+            console.log(e);
+          }
+        });
+    }
+  };
+
+  doValidation = (username, password) => {
+    if (username.trim().length === 0) {
+      this.setState({ usernameValidation: "is-invalid" });
     }
 
-    checkLoggedIn = () => {
-        if (this.state.userRole === "ROLE_ADMIN") {
-            this.props.history.push(urls.admin.userBase);
-        } else if (this.state.userRole === "ROLE_EDUCATION_SPECIALIST") {
-            this.props.history.push(urls.educationSpecialist.kindergartenBase);
-        } else if (this.state.hasDetails && this.state.userRole === "ROLE_GUARDIAN") {
-            this.props.history.push(urls.guardian.applicationBase);
-        } else if (!this.state.hasDetails && this.state.userRole === "ROLE_GUARDIAN") {
-            this.props.history.push(urls.guardian.primaryDataBase);
-        }
+    if (password.trim().length === 0) {
+      this.setState({ passwordValidation: "is-invalid" });
     }
+  };
 
-    handleChange = (e) => {
-        const { name, value } = e.target;
+  render() {
+    return (
+      <div id="loginPage" className="row container-fluid ">
+        <div className="col-5 col-xl-5 col-lg-5 col-md-5 col-sm-5 col-xs-5  ">
+          <img className="xxx " src={xxx} alt="xxx" />
+        </div>
+        <div className="col-7 col-xl-7 col-lg-7 col-md-7 col-sm-7 col-xs-7">
+          <LoginComponent
+            username={this.state.username}
+            password={this.state.password}
+            usernameValidation={this.state.usernameValidation}
+            passwordValidation={this.state.passwordValidation}
+            areCredentialsIncorrect={this.state.areCredentialsIncorrect}
+            onSubmit={this.handleSubmit}
+            onUsernameChange={this.handleChange}
+            onPasswordChange={this.handleChange}
+          />
+        </div>
+      </div>
+    );
+  }
 
-        this.setState({ [name]: value });
-
-        if (this.state.usernameValidation !== "" && name === "username") {
-            this.setState({ usernameValidation: "" });
-        }
-
-        if (this.state.passwordValidation !== "" && name === "password") {
-            this.setState({ passwordValidation: "" });
-        }
-
-        if (this.state.areCredentialsIncorrect) {
-            this.setState({ areCredentialsIncorrect: false });
-        }
-    }
-
-    resetState = () => {
-        this.setState({ username: "" });
-        this.setState({ password: "" });
-        this.setState({ usernameValidation: "" });
-        this.setState({ passwordValidation: "" });
-        this.setState({ incorrectCredentials: false });
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-
-        let usernameFromUser = e.target.username.value;
-        let passwordFromUser = e.target.password.value;
-
-        this.doValidation(usernameFromUser, passwordFromUser);
-
-        if (usernameFromUser.trim().length !== 0 && passwordFromUser.trim().length !== 0) {
-            let userData = new URLSearchParams();
-            userData.append('username', this.state.username);
-            userData.append('password', this.state.password);
-
-            axios
-                .post(`${baseUrl}/login`,
-                    userData,
-                    { headers: { 'Content-type': 'application/x-www-form-urlencoded' } })
-                .then(() => {
-                    axios
-                        .get(`${baseUrl}/loggedRole`)
-                        .then((res) => {
-                            this.setState({ userRole: res.data })
-                            if (res.data === "ROLE_GUARDIAN") {
-                                axios
-                                    .get(`${baseUrl}/loggedWithDetails`)
-                                    .then((res) => {
-                                        console.log(res.data);
-                                        this.setState({ hasDetails: res.data })
-                                    })
-                                    .then(() => this.checkLoggedIn())
-                                    .catch(err => console.log(err))
-                            } else {
-                                this.checkLoggedIn();
-                            }
-                        })
-                        .catch(err => console.log(err))
-                })
-                .catch((e) => {
-                    if (e.response.status && e.response.status === 401) {
-                        this.setState({ areCredentialsIncorrect: true });
-                    } else {
-                        console.log(e);
-                    }
-                });
-
-        }
-    }
-
-    doValidation = (username, password) => {
-        if (username.trim().length === 0) {
-            this.setState({ usernameValidation: "is-invalid" });
-        }
-
-        if (password.trim().length === 0) {
-            this.setState({ passwordValidation: "is-invalid" });
-        }
-    }
-
-    render() {
-        return (
-            <div id="loginPage" className="justify-content-center align-items-center">
-                <h1 className="text-center text-info pt-4">Vaikų darželių informacinė sistema</h1>
-                <LoginComponent
-                    username={this.state.username}
-                    password={this.state.password}
-                    usernameValidation={this.state.usernameValidation}
-                    passwordValidation={this.state.passwordValidation}
-                    areCredentialsIncorrect={this.state.areCredentialsIncorrect}
-                    onSubmit={this.handleSubmit}
-                    onUsernameChange={this.handleChange}
-                    onPasswordChange={this.handleChange}
-                />
-            </div>
-        )
-    }
+  //   render() {
+  //     return (
+  //       <div id="loginPage" className="justify-content-center align-items-center">
+  //         <h1 className="text-center text-info pt-4">
+  //           {" "}
+  //           <img className="logo" src={logo} alt="Logo" />
+  //           Vaikų darželių informacinė sistema
+  //         </h1>
+  //         <LoginComponent
+  //           username={this.state.username}
+  //           password={this.state.password}
+  //           usernameValidation={this.state.usernameValidation}
+  //           passwordValidation={this.state.passwordValidation}
+  //           areCredentialsIncorrect={this.state.areCredentialsIncorrect}
+  //           onSubmit={this.handleSubmit}
+  //           onUsernameChange={this.handleChange}
+  //           onPasswordChange={this.handleChange}
+  //         />
+  //       </div>
+  //     );
+  //   }
 }
 
 export default withRouter(LoginContainer);
