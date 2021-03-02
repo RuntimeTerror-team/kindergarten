d84acbffe3b98a4e01639a714286c773ddb2d7cf
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-03-02 14:35:43 +0200

    Added possibility to update kindergarten information

--- kindergarten-ui/src/components/KindergartenInfoForm/KindergartenInfoFormComponent.jsx
+++ kindergarten-ui/src/components/KindergartenInfoForm/KindergartenInfoFormComponent.jsx
@@ -2,96 +2,214 @@ import React from "react";
 import Proptypes from "prop-types";
 import { Link } from "react-router-dom";
 import urls from "../../constants/urls";
+import Input from "../common/Input";
 
-const KindergartenInfoFormComponent = ({ kindergarten }) => {
-  let { address, email, phoneNumber, postalCode, title, website, companyCode, district } = kindergarten;
+const KindergartenInfoFormComponent = ({
+  districts,
+  kindergarten,
+  errors,
+  handleChange,
+  handleSubmit,
+  message,
+  messageStyle,
+  isDisabled,
+  toggleDisabled
+}) => {
+  let { address, email, phoneNumber, postalCode, title, website, companyCode, district, id } = kindergarten;
+
+  let labelStyle = "col-3 pt-2 text-right";
+  let inputStyle = "col-9";
+  let invalidStyle = "offset-3 col-9";
+  let type = "text"
+
   return (
-    <form>
-      <div className="form-group row">
-        <label htmlFor="title" className="col-3 pt-2 text-right">
-          Pavadinimas
-          </label>
-        <input type="text" className="form-control col-9" id="title" name="title" value={title} readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="companyCode" className="col-3 pt-2 text-right">
-          Įmonės kodas
-          </label>
-        <input
-          type="number"
-          className="form-control col-9"
-          id="companyCode"
+    <div>
+      <form id={id} onSubmit={handleSubmit}>
+        <Input
+          name="title"
+          value={title}
+          label="Pavadinimas"
+          mandatory={true}
+          type={type}
+          error={errors.title}
+          placeholder="Pvz.: Lopšelis-darželis Voverytė"
+          onChange={handleChange}
+          errorMessage="Šis laukas privalomas. Pavadinimo ilgis turi būti 8-35 ženklų."
+          labelStyle={labelStyle}
+          inputStyle={inputStyle}
+          invalidStyle={invalidStyle}
+          disabled={isDisabled}
+        />
+        <Input
           name="companyCode"
           value={companyCode}
-          readOnly
+          label="Įmonės kodas"
+          mandatory={true}
+          type="number"
+          error={errors.companyCode}
+          placeholder="Pvz.: 123456789"
+          onChange={handleChange}
+          errorMessage="Šis laukas privalomas. Įmonės kodo ilgis turi būti 7 arba 9 skaitmenų."
+          labelStyle={labelStyle}
+          inputStyle={inputStyle}
+          invalidStyle={invalidStyle}
+          disabled={true}
         />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="address" className="col-3 pt-2 text-right">
-          Adresas
+        <Input
+          name="address"
+          value={address}
+          label="Adresas"
+          mandatory={true}
+          type={type}
+          error={errors.address}
+          placeholder="Pvz.: Kalvarijų g. 128"
+          onChange={handleChange}
+          errorMessage="Šis laukas privalomas. Adreso ilgis turi būti 8-50 ženklų."
+          labelStyle={labelStyle}
+          inputStyle={inputStyle}
+          invalidStyle={invalidStyle}
+          disabled={isDisabled}
+        />
+        {isDisabled
+          ?
+          <Input
+            name="district"
+            value={district.title}
+            label="Rajonas"
+            mandatory={true}
+            type={type}
+            error={errors.district}
+            onChange={handleChange}
+            errorMessage="Šis laukas privalomas. Adreso ilgis turi būti 8-50 ženklų."
+            labelStyle={labelStyle}
+            inputStyle={inputStyle}
+            invalidStyle={invalidStyle}
+            disabled={isDisabled}
+          />
+          : <div className="form-group row">
+            <label htmlFor="district" className="col-3 pt-2 text-right">
+              Rajonas <span className="mandatory">*</span>
+            </label>
+            <select
+              id="district"
+              className={`form-control ${inputStyle} ${errors.district}`}
+              name="district"
+              onChange={handleChange}>
+              <option>{district.title}</option>
+              {districts.filter(dis => dis.title !== district.title).map(dis => {
+                return (
+                  <option key={dis.id} value={`${dis.title}+${dis.id}`}>
+                    {dis.title}
+                  </option>
+
+                );
+              })}
+            </select>
+            <div className={`invalid-feedback ${invalidStyle}`}>Šis laukas privalomas. Pasirinkite rajoną.</div>
+          </div>}
+        <div className="form-group row">
+          <label htmlFor="city" className="col-3 pt-2 text-right">
+            Miestas
+                    </label>
+          <input type="text" className="form-control col-9" id="city" value="Vilnius" name="city" disabled />
+        </div>
+        <div className="form-group row">
+          <label htmlFor="postalCode" className="col-3 pt-2 text-right">
+            Pašto kodas {!isDisabled && <span className="mandatory">*</span>}
           </label>
-        <input type="text" className="form-control col-9" id="address" value={address} name="address" readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="district" className="col-3 pt-2 text-right">
-          Rajonas
+          <input type="text" className="form-control col-1" placeholder="LT-" readOnly />
+          <input
+            type="number"
+            className={`form-control col-8 ${errors.postalCode}`}
+            id="postalCode"
+            placeholder="00000"
+            value={postalCode}
+            name="postalCode"
+            onChange={handleChange}
+            disabled={isDisabled}
+          />
+          <div className={`invalid-feedback ${invalidStyle}`}>Šis laukas privalomas. Pašto kodo ilgis turi būti 5 skaitmenų.</div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="phoneNumber" className="col-3 pt-2 text-right">
+            Tel. numeris {!isDisabled && <span className="mandatory">*</span>}
           </label>
-        <input type="text" className="form-control col-9" id="district" value={district.title} readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="city" className="col-3 pt-2 text-right">
-          Miestas
-          </label>
-        <input type="text" className="form-control col-9" id="city" value="Vilnius" name="city" readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="postalCode" className="col-3 pt-2 text-right">
-          Pašto kodas
-          </label>
-        <input type="text" className="form-control col-1" value="LT-" readOnly />
-        <input type="text" className="form-control col-8" id="postalCode" value={postalCode} readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="phoneNumber" className="col-3 pt-2 text-right">
-          Telefono numeris
-          </label>
-        <input type="text" className="form-control col-9" id="phoneNumber" value={phoneNumber} readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="email" className="col-3 pt-2 text-right">
-          El. paštas
-          </label>
-        <input type="email" className="form-control col-9" id="email" value={email ? email : "-"} name="email" readOnly />
-      </div>
-      <div className="form-group row">
-        <label htmlFor="website" className="col-3 pt-2 text-right">
-          Interneto svetainė
-          </label>
-        <input
-          type="url"
-          className="form-control col-9"
-          id="website"
-          value={website ? website : "-"}
+          <input type="text" className="form-control col-1 p-1" placeholder="+370" readOnly />
+          <input
+            type="number"
+            className={`form-control col-8 ${errors.phoneNumber}`}
+            id="phoneNumber"
+            placeholder="60000000"
+            value={phoneNumber}
+            name="phoneNumber"
+            onChange={handleChange}
+            disabled={isDisabled}
+          />
+          <div className="col-3"></div>
+          <div className="invalid-feedback col-9">Šis laukas privalomas. Telefono numeris ilgis turi būti 8 skaitmenų.</div>
+        </div>
+        <Input
+          name="email"
+          value={email}
+          label="El. paštas"
+          mandatory={false}
+          type={type}
+          error={true}
+          placeholder="pavyzdys@pvz.lt"
+          onChange={handleChange}
+          errorMessage="Įveskite elektroninį paštą arba palikite tuščią."
+          labelStyle={labelStyle}
+          inputStyle={inputStyle}
+          invalidStyle={invalidStyle}
+          disabled={isDisabled}
+        />
+        <Input
           name="website"
-          readOnly
+          value={website}
+          label="Interneto svetainė"
+          mandatory={false}
+          type={type}
+          error={true}
+          placeholder="www.pavyzdys.lt"
+          onChange={handleChange}
+          errorMessage="Įveskite svetainės adresą arba palikite tuščią."
+          labelStyle={labelStyle}
+          inputStyle={inputStyle}
+          invalidStyle={invalidStyle}
+          disabled={isDisabled}
         />
-      </div>
+        <div className="col-12 row">
+          {!isDisabled && <div className="offset-3 col-9"><span className="mandatory">*</span> pažymėti laukai privalomi.</div>}
+          {message && <div className={`${messageStyle} offset-4 col-8 mt-4`}>
+            {message}
+          </div>}
+        </div>
+      </form>
+      {
+        isDisabled
+          ? <button type="button" className="btn btn-green float-right ml-2" onClick={toggleDisabled} >
+            Redaguoti
+      </button>
+          : <button type="submit" className="btn btn-green float-right ml-2" onClick={handleSubmit}>
+            Išsaugoti
+    </button>
+      }
       <Link className="btn btn-warning float-right" to={`${urls.educationSpecialist.kindergartenBase}`}>
-        Atgal
+        Grįžti į darželių sąrašą
       </Link>
-    </form>
+    </div>
   );
 };
 
 KindergartenInfoFormComponent.propsTypes = {
-  address: Proptypes.string.required,
-  email: Proptypes.string.required,
-  phoneNumber: Proptypes.string.required,
-  postalCode: Proptypes.string.required,
-  title: Proptypes.string.required,
-  website: Proptypes.string.required,
-  companyCode: Proptypes.string.required,
-  district: Proptypes.object.required
+  kindergarten: Proptypes.object.required,
+  district: Proptypes.object.required,
+  districts: Proptypes.array.isRequired,
+  handleChange: Proptypes.func.isRequired,
+  handleSubmit: Proptypes.func.isRequired,
+  message: Proptypes.string.isRequired,
+  messageStyle: Proptypes.string.isRequired,
+  errors: Proptypes.object.isRequired
 }
 
 export default KindergartenInfoFormComponent;
--- kindergarten-ui/src/components/KindergartenInfoForm/KindergartenInfoFormContainer.jsx
+++ kindergarten-ui/src/components/KindergartenInfoForm/KindergartenInfoFormContainer.jsx
@@ -11,7 +11,21 @@ class KindergartenInfoFormContainer extends Component {
         super(props);
         this.state = {
             districts: [],
-            kindergarten: null
+            kindergarten: {
+                title: "",
+                companyCode: "",
+                address: "",
+                district: "",
+                postalCode: "",
+                phoneNumber: "",
+                email: "",
+                website: "",
+                id: ""
+            },
+            errors: {},
+            message: "",
+            messageStyle: "",
+            isDisabled: true
         }
     }
 
@@ -19,9 +33,186 @@ class KindergartenInfoFormContainer extends Component {
         axios
             .get(`${baseUrl}/api/kindergartens/${this.props.match.params.id}`)
             .then((res) => {
-                this.setState({ kindergarten: res.data })
+                let kindergarten = {
+                    title: res.data.title,
+                    companyCode: res.data.companyCode,
+                    address: res.data.address,
+                    district: res.data.district,
+                    postalCode: res.data.postalCode,
+                    phoneNumber: res.data.phoneNumber.substr(res.data.phoneNumber.length - 8),
+                    email: res.data.email,
+                    website: res.data.website,
+                    id: res.data.id
+                }
+                this.setState({ kindergarten: kindergarten })
             })
             .catch((err) => console.log(err));
+
+        axios
+            .get(`${baseUrl}/api/districts`)
+            .then((res) => {
+                this.setState({ districts: res.data })
+            })
+            .catch((err) => console.log(err));
+    }
+
+    handleChange = ({ target: input }) => {
+        const errors = { ...this.state.errors };
+        const errorMessage = this.validateProperty(input);
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
+        const kindergarten = { ...this.state.kindergarten };
+        kindergarten[input.name] = input.value;
+        this.setState({ kindergarten, errors });
+
+        this.setState({ message: "", messageStyle: "" });
+    }
+
+    validateProperty = ({ name, value }) => {
+        if (name === "title") {
+            if (value.trim().length < 8 || value.trim().length > 35)
+                return "is-invalid";
+        }
+
+        if (name === "companyCode") {
+            if (value.trim().length !== 7 && value.trim().length !== 9)
+                return "is-invalid";
+        }
+
+        if (name === "address") {
+            if (value.trim().length < 8 && value.trim().length > 50)
+                return "is-invalid";
+        }
+
+        if (name === "district") {
+            if (value === "Pasirinkti..." || value === "")
+                return "is-invalid";
+        }
+
+        if (name === "postalCode") {
+            if (value.trim().length !== 5)
+                return "is-invalid";
+        }
+
+        if (name === "phoneNumber") {
+            if (value.trim().length !== 8)
+                return "is-invalid";
+        }
+
+        if (name === "email") {
+            const re = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+            if (!re.test(value) && value.trim() !== "")
+                return "is-invalid";
+        }
+
+        if (name === "website") {
+            const re = /^((https?):\/\/)?([w|W]{3}\.)+[a-zA-Z0-9\-.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
+            if (!re.test(value) && value.trim() !== "")
+                return "is-invalid";
+        }
+    }
+
+    validate = () => {
+        const errors = {};
+
+        const { kindergarten } = this.state;
+        if (kindergarten.title.trim() === '')
+            errors.title = "is-invalid"
+
+        if (kindergarten.companyCode.trim() === '')
+            errors.companyCode = "is-invalid"
+
+        if (kindergarten.district === '' || kindergarten.district === 'Pasirinkti...')
+            errors.district = "is-invalid"
+
+        if (kindergarten.address.trim() === '')
+            errors.address = "is-invalid"
+
+        if (kindergarten.postalCode.trim() === '')
+            errors.postalCode = "is-invalid"
+
+        if (kindergarten.phoneNumber.trim() === '')
+            errors.phoneNumber = "is-invalid"
+
+        return Object.keys(errors).length === 0 ? null : errors;
+    }
+
+    toggleDisabled = () => {
+        if (this.state.isDisabled === true) {
+            this.setState({
+                message: "",
+                messageStyle: ""
+            })
+        }
+        this.setState({ isDisabled: !this.state.isDisabled })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { kindergarten } = this.state;
+
+        const errors = this.validate();
+        this.setState({ errors: errors || {} });
+        if (errors) {
+            this.setState({ message: "Darželio informacijos atnaujinti nepavyko. Pasitikrinkite duomenis." })
+            this.setState({ messageStyle: "alert alert-danger" })
+            return;
+        }
+
+        console.log(kindergarten);
+        // console.log(typeof kindergarten.district === 'string' && kindergarten.district.split[1]);
+        // console.log(kindergarten.district.split("+")[0]);
+
+        axios
+            .put(`${baseUrl}/api/kindergartens/${kindergarten.id}`, {
+                "address": kindergarten.address,
+                "city": "VILNIUS",
+                "companyCode": kindergarten.companyCode,
+                "district": {
+                    id: typeof kindergarten.district === 'string'
+                        ? kindergarten.district.split("+")[1]
+                        : kindergarten.district.id,
+                    title: typeof kindergarten.district === 'string'
+                        ? kindergarten.district.split("+")[0]
+                        : kindergarten.district.title
+                },
+                "email": kindergarten.email,
+                "phoneNumber": "+370" + kindergarten.phoneNumber,
+                "postalCode": kindergarten.postalCode,
+                "title": kindergarten.title,
+                "website": kindergarten.website
+            })
+            .then(() => {
+                this.toggleDisabled();
+                this.setState({ message: "Darželio informacija sėkmingai atnaujinta" })
+                this.setState({ messageStyle: "alert alert-success" })
+                axios
+                    .get(`${baseUrl}/api/kindergartens/${this.props.match.params.id}`)
+                    .then((res) => {
+                        let kindergarten = {
+                            title: res.data.title,
+                            companyCode: res.data.companyCode,
+                            address: res.data.address,
+                            district: res.data.district,
+                            postalCode: res.data.postalCode,
+                            phoneNumber: res.data.phoneNumber.substr(res.data.phoneNumber.length - 8),
+                            email: res.data.email,
+                            website: res.data.website,
+                            id: res.data.id
+                        }
+                        this.setState({ kindergarten: kindergarten })
+                    })
+                    .catch((err) => console.log(err));
+            })
+            .catch((err) => {
+                if (err.response.status && err.response.status === 409) {
+                    this.setState({ message: err.response.data })
+                    this.setState({ messageStyle: "alert alert-danger" })
+                } else {
+                    console.log(err);
+                }
+            });
     }
 
     render() {
@@ -38,8 +229,13 @@ class KindergartenInfoFormContainer extends Component {
                                     <KindergartenInfoFormComponent
                                         districts={this.state.districts}
                                         kindergarten={this.state.kindergarten}
-                                        resetWantsInfo={this.props.resetWantsInfo}
-                                        otherProps={this.state}
+                                        errors={this.state.errors}
+                                        handleChange={this.handleChange}
+                                        handleSubmit={this.handleSubmit}
+                                        message={this.state.message}
+                                        messageStyle={this.state.messageStyle}
+                                        isDisabled={this.state.isDisabled}
+                                        toggleDisabled={this.toggleDisabled}
                                     />
                                 </div>
                             </div>
@@ -47,7 +243,6 @@ class KindergartenInfoFormContainer extends Component {
                         <Footer />
                     </div>
                 </div>
-
             )
         } else {
             return (
