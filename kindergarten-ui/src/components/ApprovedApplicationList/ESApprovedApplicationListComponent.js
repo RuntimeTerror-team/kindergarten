99b5b8cac2426fe2a1847a0ca48e8b44ffe55918
gnar-404 <75095108+gnar-404@users.noreply.github.com>
2021-03-03 19:06:32 +0200

    Rt 167 trigger queue recalculation (#127)
    * add recalculate applications status
    
    * sorting works ok
    
    * edit dataseed
    
    * edit naming
    
    * add button

--- kindergarten-ui/src/components/ApprovedApplicationList/ESApprovedApplicationListComponent.js
+++ kindergarten-ui/src/components/ApprovedApplicationList/ESApprovedApplicationListComponent.js
@@ -1,69 +1,83 @@
-import React from 'react'
-import Proptypes from 'prop-types';
+import React from "react";
+import Proptypes from "prop-types";
 
-const ESApprovedApplicationListComponent = ({ applications }) => {
+const ESApprovedApplicationListComponent = ({ applications, recalculation }) => {
+  let table = (
+    <div className="col-12 mt-3">
+      <table id="groupTable" className="table col-12">
+        <thead>
+          <tr>
+            {/* <th scope='col' style={{ width: "30px" }}>#</th> */}
+            <th scope="col" style={{ width: "60px" }}>
+              Balas
+            </th>
+            <th scope="col">Vaikas</th>
+            <th scope="col">Tėvelis</th>
+            <th scope="col" style={{ width: "115px" }}>
+              Data
+            </th>
+            <th scope="col">Statusas</th>
+            <th scope="col">Darželis</th>
+            <th scope="col" style={{ width: "115px" }}>
+              Laukimo Nr
+            </th>
+          </tr>
+        </thead>
 
-    let table = (
-        <div className="col-12 mt-3">
-            <table id="groupTable" className='table col-12'>
-                <thead>
-                    <tr>
-                        {/* <th scope='col' style={{ width: "30px" }}>#</th> */}
-                        <th scope='col' style={{"width": "60px"}}>Balas</th>
-                        <th scope='col'>Vaikas</th>
-                        <th scope='col'>Tėvelis</th>
-                        <th scope='col' style={{"width": "115px"}}>Data</th>
-                        <th scope='col'>Statusas</th>
-                        <th scope='col'>Darželis</th>
-                        <th scope='col' style={{"width": "115px"}}>Laukimo Nr</th>
-                    </tr>
-                </thead>
+        <tbody>
+          {applications.map(
+            (
+              {
+                id,
+                score,
+                childFirstName,
+                childLastName,
+                parentFirstName,
+                parentLastName,
+                date,
+                status,
+                approvedKindergartenTitle,
+                waitingNumber,
+              },
+              index
+            ) => {
+              if (waitingNumber === null) {
+                waitingNumber = "-";
+              }
 
-                <tbody>
-                    {applications.map(({ id, score, childFirstName, childLastName, parentFirstName, parentLastName, date, status,
-                     approvedKindergartenTitle, waitingNumber  }, index) => {
+              if (approvedKindergartenTitle === null) {
+                approvedKindergartenTitle = "-";
+              }
 
-                        if(waitingNumber === null){
-                            waitingNumber = "-"
-                        }
+              return (
+                <tr key={id}>
+                  {/* <th scope='row'>{index + 1}</th> */}
+                  <td>{score}</td>
+                  <td>{childFirstName + " " + childLastName}</td>
+                  <td>{parentFirstName + " " + parentLastName}</td>
+                  <td>{date}</td>
+                  <td>{status}</td>
+                  <td>{approvedKindergartenTitle}</td>
+                  <td>{waitingNumber}</td>
+                </tr>
+              );
+            }
+          )}
+        </tbody>
+      </table>
+      <div className="pb-5">
+        <button type="submit" className="btn btn-yellow float-right" onClick={recalculation}>
+          Perūšiuoti prašymus
+        </button>
+      </div>
+    </div>
+  );
 
-                        if(approvedKindergartenTitle === null){
-                            approvedKindergartenTitle = "-"
-                        }
-                        
-                        return(
-
-                            <tr key={id}>
-                            {/* <th scope='row'>{index + 1}</th> */}
-                            <td>{score}</td>
-                            <td>{childFirstName + " " + childLastName}</td>
-                            <td>{parentFirstName + " " + parentLastName}</td>
-                            <td>{date}</td>
-                            <td>{status}</td>
-                            <td>{approvedKindergartenTitle}</td>
-                            <td>{waitingNumber}</td>
-                        </tr>
-                        )
-
-                     }
-                    )}
-                </tbody>
-            </table>
-        </div>
-
-    )
-
-
-    return (
-
-        applications.length === 0 ? <h6 className="text-center">Prašymų registracija nėra sustabdyta</h6> : table
-    )
-
-     
-}
+  return applications.length === 0 ? <h6 className="text-center">Prašymų registracija nėra sustabdyta</h6> : table;
+};
 
 ESApprovedApplicationListComponent.propTypes = {
-    applications: Proptypes.array
-}
+  applications: Proptypes.array,
+};
 
-export default ESApprovedApplicationListComponent
+export default ESApprovedApplicationListComponent;
