2641e4c83c5e575b621d3a49bbf28e4830aa5e55
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-03-03 17:12:22 +0200

    add button

--- kindergarten-ui/src/components/ApprovedApplicationList/ESApprovedApplicationListContainer.js
+++ kindergarten-ui/src/components/ApprovedApplicationList/ESApprovedApplicationListContainer.js
@@ -1,86 +1,81 @@
-import Axios from 'axios';
-import React, { Component } from 'react';
-import baseUrl from '../../AppConfig'
-import ESNavigationComponent from '../Navigation/ESNavigationComponent'
-import HeaderComponent from '../Header/HeaderComponent';
-import ESApprovedApplicationListComponent from './ESApprovedApplicationListComponent'
-import Footer from '../Footer/Footer';
+import Axios from "axios";
+import React, { Component } from "react";
+import baseUrl from "../../AppConfig";
+import ESNavigationComponent from "../Navigation/ESNavigationComponent";
+import HeaderComponent from "../Header/HeaderComponent";
+import ESApprovedApplicationListComponent from "./ESApprovedApplicationListComponent";
+import Footer from "../Footer/Footer";
 
-class ESApprovedApplicationListContainer extends Component{
+class ESApprovedApplicationListContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      applications: [],
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
+  componentDidMount() {
+    this.updateApplicationList();
+  }
 
-            applications: [],
-        }
-}
+  updateApplicationList() {
+    Axios.get(baseUrl + "/api/applications/sorted")
+      .then((res) => {
+        this.setState({ applications: res.data });
+        this.translateStatus();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-componentDidMount(){
+  translateStatus() {
+    this.state.applications.forEach((application) => {
+      if (application.status === "SUBMITTED") {
+        application.status = "Pateiktas";
+        this.forceUpdate();
+      } else if (application.status === "UNCOMFIRMED") {
+        application.status = "Nepatvirtintas";
+        this.forceUpdate();
+      } else if (application.status === "REJECTED") {
+        application.status = "Atmestas";
+        this.forceUpdate();
+      } else if (application.status === "APPROVED") {
+        console.log("Approved");
+        application.status = "Patvirtintas";
+        this.forceUpdate();
+      } else if (application.status === "WAITING") {
+        application.status = "Eilėje";
+        this.forceUpdate();
+      }
+    });
+  }
 
-    Axios
-    .get(baseUrl + "/api/applications/sorted")
-       .then(res => {
-           this.setState({applications: res.data});
-           this.translateStatus();
-        })
-       .catch(err => {console.log(err)})
-}
+  recalculateApplications = async () => {
+    await Axios.post(`${baseUrl}/api/applications/recalculation`).catch((err) => console.log(err));
+    this.updateApplicationList();
+  };
 
-translateStatus(){
-
-    this.state.applications.forEach(application => {
-
-        if(application.status === "SUBMITTED"){
-            application.status = 'Pateiktas'
-            this.forceUpdate()
-        }
-
-        else if(application.status === "UNCOMFIRMED"){
-            application.status = 'Nepatvirtintas'
-            this.forceUpdate()
-        }
-
-        else if(application.status === "REJECTED"){
-            application.status = 'Atmestas'
-            this.forceUpdate()
-        }
-
-        else if(application.status === "APPROVED"){
-
-            console.log("Approved")
-            application.status = 'Patvirtintas'
-            this.forceUpdate()
-        }
-
-        else if(application.status === "WAITING"){
-            application.status = 'Eilėje'
-            this.forceUpdate()
-        }
-    })
-}
-
-render(){
-
-
+  render() {
     return (
-        <div className="footerBottom">
-            <HeaderComponent userRole="ROLE_EDUCATION_SPECIALIST"/>
-            <div className="container py-4">
-                <div className="row">
-                    <ESNavigationComponent />
-                    <div className="col-8">
-                        <h1 className="mb-5 text-center">Prašymai</h1>
-                        <ESApprovedApplicationListComponent
-                         applications={this.state.applications}/>
-                    </div>
-                </div>
+      <div className="footerBottom">
+        <HeaderComponent userRole="ROLE_EDUCATION_SPECIALIST" />
+        <div className="container py-4">
+          <div className="row">
+            <ESNavigationComponent />
+            <div className="col-8">
+              <h1 className="mb-5 text-center">Prašymai</h1>
+              <ESApprovedApplicationListComponent
+                applications={this.state.applications}
+                recalculation={this.recalculateApplications}
+              />
             </div>
-            <Footer />
+          </div>
         </div>
-    )
-}
-
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default ESApprovedApplicationListContainer;
