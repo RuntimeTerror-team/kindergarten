2968d308bd575051512fde456b477a3beac6121f
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-03-07 14:56:10 +0200

    Hashed role in local storage

--- kindergarten-ui/src/components/Route/PrivateRoute.jsx
+++ kindergarten-ui/src/components/Route/PrivateRoute.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { isLogin, getLoggedInRole } from '../../utils/utils';
+import { isLogin, getLoggedInRole, hashCode } from '../../utils/utils';
 
 const PrivateRoute = ({ component: Component, userRole, ...rest }) => {
     return (
         // Show the component only when the user is logged in
-        // Otherwise, redirect the user to homepage / to login
+        // Otherwise, redirect the user to not authorized page /not-authorized to inform and suggest logging in
         <Route {...rest} render={props => (
-            isLogin() && userRole === getLoggedInRole() ?
+            isLogin() && hashCode(userRole) === +getLoggedInRole() ?
                 <Component {...props} />
                 : <Redirect to="/not-authorized" />
         )} />
--- kindergarten-ui/src/utils/utils.js
+++ kindergarten-ui/src/utils/utils.js
@@ -1,7 +1,8 @@
 const TOKEN_KEY = 'jwt';
 
 export const login = (role) => {
-    localStorage.setItem(TOKEN_KEY, role);
+    let hashedRole = hashCode(role);
+    localStorage.setItem(TOKEN_KEY, hashedRole);
 }
 
 export const logout = () => {
@@ -18,3 +19,5 @@ export const isLogin = () => {
 export const getLoggedInRole = () => {
     return localStorage.getItem(TOKEN_KEY);
 }
+
+export const hashCode = (s) => s.split('').reduce((a, b) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0)
