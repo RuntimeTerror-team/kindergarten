d5c5bf43317de9f67f4ccb76f5e3cc70cf47bfd4
Laura Stasiule <61902552+Lamista@users.noreply.github.com>
2021-03-08 21:40:37 +0200

    Changed styles for left navigation links

--- kindergarten-ui/src/components/Navigation/AdminNavigationComponent.jsx
+++ kindergarten-ui/src/components/Navigation/AdminNavigationComponent.jsx
@@ -14,13 +14,13 @@ const AdminNavigationComponent = () => {
             </div>
             <nav className="templatemo-left-nav">
                 <Link to={`${urls.admin.userBase}`}
-                    className={`${window.location.pathname === `/kindergarten${urls.admin.userBase}` ? "active" : ""}`}>
+                    className={`${window.location.pathname === `/kindergarten${urls.admin.userBase}` ? "active" : ""} left-nav-link`}>
                     Paskyrų administravimas</Link>
                 <Link to={`${urls.admin.districtBase}`}
-                    className={`${window.location.pathname === `/kindergarten${urls.admin.districtBase}` ? "active" : ""}`}>
+                    className={`${window.location.pathname === `/kindergarten${urls.admin.districtBase}` ? "active" : ""} left-nav-link`}>
                     Rajonų administravimas</Link>
                 <Link to={`${urls.admin.queueBase}`}
-                    className={`${window.location.pathname === `/kindergarten${urls.admin.queueBase}` ? "active" : ""}`}>
+                    className={`${window.location.pathname === `/kindergarten${urls.admin.queueBase}` ? "active" : ""} left-nav-link`}>
                     Eilių administravimas </Link>
             </nav>
         </div >
