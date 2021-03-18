976adcc8c577672b12a0082567b911f442194413
Gintare Remeikiene <gin.remeikiene@gmail.com>
2021-03-18 02:29:21 +0200

    partial commit

--- kindergarten/src/main/java/lt/vtmc/kindergarten/CommandLineAppStartupRunner.java
+++ kindergarten/src/main/java/lt/vtmc/kindergarten/CommandLineAppStartupRunner.java
@@ -114,13 +114,13 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
 
             List<String> guardianPersonalCodes = new ArrayList<>();
-            for (int i = 0; i < 250; i++) {
+            for (int i = 0; i < 150; i++) {
                 String createdPersonalCode = dataSeeder.createPerson(i, "GUARDIAN");
                 guardianPersonalCodes.add(createdPersonalCode);
             }
 
             List<String> childrenPersonalCodes = new ArrayList<>();
-            for (int i = 0; i < 750; i++) {
+            for (int i = 0; i < 450; i++) {
                 String createdPersonalCode = dataSeeder.createPerson(i, "NON_GUARDIAN");
                 childrenPersonalCodes.add(createdPersonalCode);
             }
